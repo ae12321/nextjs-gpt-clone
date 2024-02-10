d1bc8466eccb6af8770f87883a40eea97aceee77
akira.m1 <80916217+ae12321@users.noreply.github.com>
2024-02-10 21:10:40 +0900

    Update: 2024-02-10 21:10:40

--- app/client/page.jsx
+++ app/client/page.jsx
@@ -1,9 +1,22 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 
 export default function ClientPage() {
+  const [count, setCount] = useState(0);
+  console.log("count");
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+
   return (
     <div>
-      <h1 className="text-5xl">ClientPage</h1>
+      <h1 className="mb-4">ClientPage</h1>
+      <p className="text-3xl font-bold mb-4">{count}</p>
+      <button className="btn btn-secondary" onClick={increment}>
+        Increment
+      </button>
     </div>
   );
 }
