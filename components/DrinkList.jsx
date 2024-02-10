dd9e1e3ebd5271e43d8f355f556bc74654a39cd9
akira.m1 <80916217+ae12321@users.noreply.github.com>
2024-02-10 22:16:42 +0900

    Update: 2024-02-10 22:16:41

--- components/DrinkList.jsx
+++ components/DrinkList.jsx
@@ -1,16 +1,20 @@
+import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
 export default function DrinkList({ drinks }) {
   return (
     <div>
-      <ul className="menu menu-vertical">
+      <ul className="grid md:grid-cols-2">
         {drinks.map((drink, index) => {
           const { idDrink: id, strDrink: name, strDrinkThumb: thumbnail } = drink;
           console.log({ id, name, thumbnail });
           return (
-            <li key={id} className="text-xl">
-              <Link href={`/server/${id}`}>{name}</Link>
+            <li key={id}>
+              <Link href={`/server/${id}`} className="text-xl flex flex-col">
+                {name}
+                <Image alt={name} src={thumbnail} width={100} height={100} />
+              </Link>
             </li>
           );
         })}
